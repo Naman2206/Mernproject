c031a45043d520951a0c60d0174bb05eeb618e4a
Naman2206 <namanmehta226@gmail.com>
2023-07-20 20:59:00 +0530

    others

--- server/app.js
+++ server/app.js
@@ -1,17 +1,23 @@
 const express=require('express');
 const app=express();
 
+const middleware =(req,res,next)=> {
+    console.log("my middleware");
+    next();
+}
+
+
 app.get('/',(req,res)=>{
     res.send(`hello world from the server`)
 });
-app.get('/about',(req,res)=>{
-    res.send(`hello About world from the server`)
+app.get('/about',middleware,(req,res)=>{
+    res.send(`hello About world from the server`);
 });
 app.get('/contact',(req,res)=>{
-    res.send(`hello contact world from the server`)
+    res.send(`hello contact world from the server`);
 });
 app.get('/signin',(req,res)=>{
-    res.send(`hello Login world from the server`)
+    res.send(`hello Login world from the server`);
 });
 app.get('/signup',(req,res)=>{
     res.send(`hello Registration world from the server`)
